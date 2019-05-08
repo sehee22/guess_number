8e047851cdbb02cbc5bd4a83c956d3d8086de8db
sehee22 <40151468+sehee22@users.noreply.github.com>
2019-05-08 10:44:45 -0400

    reset guess field

--- guess.js
+++ guess.js
@@ -18,16 +18,19 @@ function decideIfwon() {
         console.log('too high');
         lowOrHiMessage.textContent  = 'too high';
         guessMessage.textContent = guessMessage.textContent + ' ' + guess.value;
+        guess.value = '';
     }
     else if (guess.value < price){
         console.log('too low');
         lowOrHiMessage.textContent  = 'too low';
         guessMessage.textContent = guessMessage.textContent + ' ' + guess.value;
+        guess.value = '';
     }
     else {
         console.log ('winner')
         lowOrHiMessage.textContent  = 'winner!';
         guessMessage.textContent = guessMessage.textContent + ' ' + guess.value;
+        guess.value = '';
     }
 }
 
